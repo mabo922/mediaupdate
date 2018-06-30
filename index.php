b6f6460134425b55164a25e4c46112f096cb77b0
mabo922 <33652593+mabo922@users.noreply.github.com>
2018-07-01 00:33:52 +0900

    <new change>

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$signature = $_SERVER ['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$events = $bot -> parseEventRequest(file_get_contents('php://input'),$signature);
+foreach ($events as $event){
+  $bot ->replyText($event->getReplyToken(), 'TextMessage');
+}
+
+
 
  ?>
